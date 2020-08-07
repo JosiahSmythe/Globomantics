4f00e6a0e13b91cbe4f69d0318a99e5a45722e86
Josiah G. Smythe <jsmythe@DEVBASELINE>
2020-08-07 09:42:08 -0400

    Added shared library for the models

---
+++ Shared/ConferenceModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading;
+
+namespace Shared
+{
+    public class ConferenceModel
+    {
+        public ConferenceModel()
+        {
+            Start = DateTime.Now;
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Start { get; set; }
+        public string Location { get; set; }
+        [DisplayName("Attendee total")]
+        public int AttendeeTotal { get; set; }
+    }
+}
